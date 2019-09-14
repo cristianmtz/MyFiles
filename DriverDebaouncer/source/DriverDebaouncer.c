ffe8c2a0162a7ee9e3002400592935ced7e7b4fb
Cristian Martinez <cristianalex0117@hotmail.com>
2019-09-13 19:21:17 -0500

    New Proyect

---
+++ DriverDebaouncer/source/DriverDebaouncer.c
@@ -0,0 +1,46 @@
+/********************************************************
+*	Author: 											*
+*	File Name:      									*
+*   Description:    									*
+*   													*
+*											 			*
+*														*
+********************************************************/
+
+/****** Inlcude Section *******/
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MKL27Z644.h"
+#include "fsl_debug_console.h"
+
+/****** Global Variables *******/
+
+
+/****** Local Variables *******/
+
+
+/****** Local Functions *******/
+
+/** @brief
+ *  @param
+ *  @param
+ *  @return
+ */
+
+
+/****** Global Functions *******/
+
+/** @brief
+ *  @param
+ *  @param
+ *  @return
+ */
+
+int main(void) {
+
+
+    return 0 ;
+}
